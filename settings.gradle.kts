264251461db3e25d2ebabe8cf9556765d40977da
AnotherDearest <Ai_LEntropy@126.com>
2026-07-26 01:52:25 +0800

    build: 增加依赖仓库与 TLS 配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,29 @@
 pluginManagement {
     repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
         mavenLocal()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven {
             url = uri("https://mirrors.tencent.com/nexus/repository/maven-tencent/")
         }
+        google()
+        gradlePluginPortal()
+        mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
         mavenLocal()
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven {
             url = uri("https://mirrors.tencent.com/nexus/repository/maven-tencent/")
         }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
